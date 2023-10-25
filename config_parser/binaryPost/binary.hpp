b8dc7dd64f6c4275b058c8931f13b1b6a1e6b09d
Julien Kroger <jkroger@c2s10d45.42wolfsburg.de>
2023-10-25 13:21:10 +0200

    get filename and filetype

--- config_parser/binaryPost/binary.hpp
+++ config_parser/binaryPost/binary.hpp
@@ -4,23 +4,31 @@
 # include <string>
 # include <fstream>
 # include <vector>
+# include <map>
+#include <cstddef>
+#include <cstdint>
 
 class Binary {
 	public:
 		Binary();
 		~Binary();
 		void	postRequest(std::string contentType, std::vector<uint8_t> fileData);
+		void	cutFileEnd();
+		void	cutFileStart();
 	private:
+		void	_initExtensionMap();
+		void	_checkIfFileTypeExisting();
 		void	_getBinDataOfFile(std::ofstream &outfile);
 		void					_getFileName(const std::string& contentType);
 		void					_mainBinary();
 		void					_getBoundary();
-		std::string				_boundary;
-		std::string				_fileName;
-		std::string 			_fileType;
-		std::vector<uint8_t>	_requestData;
-		bool					_isMultiPart;
-		std::string				_request;
+		std::string							_boundary;
+		std::string							_fileName;
+		std::string 						_fileType;
+		std::map<std::string, std::string>	_fileTypes;
+		std::vector<uint8_t>				_requestData;
+		bool								_isMultiPart;
+		std::string							_request;
 };
 
 #endif
